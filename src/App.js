3bae7389c08f2c7f7d42709e3074b6d8089badf3
A <ajaygeorge@me.com>
2023-03-09 22:57:49 +0000

    Included chatGPT code in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import { useState } from 'react';
 import './App.css';
+import generateResume from './ChatGPT';
+import axios from 'axios';
 
 function App() {
   const [showResumeFields, setShowResumeFields] = useState(false);
   const [showCoverLetterFields, setShowCoverLetterFields] = useState(false);
+  const [output, setOutput] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const resume = document.getElementById('current-resume').value;
+    const jobDescription = document.getElementById('job-description').value;
+    const specialRequests = document.getElementById('special-requests').value;
+
+    const response = await generateResume(resume, jobDescription, specialRequests);
+    setOutput(response);
+  }
+
+  const generateCoverLetter = async () => {
+    const name = document.getElementById('cover-letter-name').value;
+    const company = document.getElementById('cover-letter-company').value;
+    const content = document.getElementById('cover-letter-content').value;
+
+    const response = await axios.post('/api/generateCoverLetter', {
+      name,
+      company,
+      content,
+    });
+
+    setOutput(response.data);
+  };
 
   return (
     <div className="App">
@@ -18,16 +46,20 @@ function App() {
           <button onClick={() => {setShowCoverLetterFields(true); setShowResumeFields(false)}}>Cover Letter</button>
         </div>
         {showResumeFields && (
-          <div className="text-fields-container">
-            <label htmlFor="current-resume">Current Resume</label>
-            <textarea type="text" id="current-resume" className="large-textarea" placeholder="Enter your current resume"></textarea>
+          <form onSubmit={handleSubmit}>
+            <div className="text-fields-container">
+              <label htmlFor="current-resume">Current Resume</label>
+              <textarea type="text" id="current-resume" className="large-textarea" placeholder="Enter your current resume"></textarea>
 
-            <label htmlFor="job-description">Description and details of job you are applying to</label>
-            <textarea type="text" id="job-description" className="large-textarea" placeholder="Enter the job description"></textarea>
+              <label htmlFor="job-description">Description and details of job you are applying to</label>
+              <textarea type="text" id="job-description" className="large-textarea" placeholder="Enter the job description"></textarea>
 
-            <label htmlFor="special-requests">Any special requests</label>
-            <textarea id="special-requests" className="large-textarea" placeholder="Enter any special requests"></textarea>
-          </div>
+              <label htmlFor="special-requests">Any special requests</label>
+              <textarea id="special-requests" className="large-textarea" placeholder="Enter any special requests"></textarea>
+
+              <button type="submit">Generate Resume</button>
+            </div>
+          </form>
         )}
         {showCoverLetterFields && (
           <div className="form-fields-container">
@@ -39,14 +71,17 @@ function App() {
 
             <label htmlFor="cover-letter-content">Content</label>
             <textarea id="cover-letter-content" className="large-textarea" placeholder="Enter your cover letter content"></textarea>
+
+            <button onClick={generateCoverLetter}>Generate Cover Letter</button>
           </div>
         )}
         <div className="output-container">
-          {/* The output from ChatGPT will be printed here */}
+          {output}
         </div>
       </header>
     </div>
   );
+
 }
 
 export default App;
