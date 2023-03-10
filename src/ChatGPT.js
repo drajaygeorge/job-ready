bd77ccddf42f7bd41f4ae60b47d7e45e88c96008
A <ajaygeorge@me.com>
2023-03-10 15:47:19 +0000

    ChatGPT.js updates

--- src/ChatGPT.js
+++ src/ChatGPT.js
@@ -3,15 +3,23 @@ import axios from 'axios';
 const API_ENDPOINT = '//api.openai.com/v1/completions'; // Replace this with your actual API endpoint
 // API Secret key
 async function generateResume(resume, jobDescription, specialRequests) {
+  console.log('Sending request...'); // Log that the request is being sent
   try {
     const response = await axios.post(API_ENDPOINT, {
       prompt: `Generate a resume for a ${jobDescription} position.\n\nResume: ${resume}\n\nSpecial requests: ${specialRequests}`,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${API_SECRET_KEY}`
+      }
     });
 
+    console.log(response.data); // Log the response data to the console
     return response.data.choices[0].text;
   } catch (error) {
     console.error(error);
     return 'Error generating resume';
+  } finally {
+    console.log('Request completed.'); // Log that the request is completed, whether successful or not
   }
 }
 
